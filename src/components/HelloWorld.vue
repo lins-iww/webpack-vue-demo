7348551198bef0838e7b336c759350d68d924bd7
linlepeng <linlepeng@igengmei.com>
2021-03-09 14:30:45 +0800

    123123

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,33 +7,25 @@
         <a
           href="https://vuejs.org"
           target="_blank"
-        >
-          Core Docs
-        </a>
+        > Core Docs </a>
       </li>
       <li>
         <a
           href="https://forum.vuejs.org"
           target="_blank"
-        >
-          Forum
-        </a>
+        > Forum </a>
       </li>
       <li>
         <a
           href="https://chat.vuejs.org"
           target="_blank"
-        >
-          Community Chat
-        </a>
+        > Community Chat </a>
       </li>
       <li>
         <a
           href="https://twitter.com/vuejs"
           target="_blank"
-        >
-          Twitter
-        </a>
+        > Twitter </a>
       </li>
       <br>
       <li>
@@ -51,33 +43,25 @@
         <a
           href="http://router.vuejs.org/"
           target="_blank"
-        >
-          vue-router
-        </a>
+        > vue-router </a>
       </li>
       <li>
         <a
           href="http://vuex.vuejs.org/"
           target="_blank"
-        >
-          vuex
-        </a>
+        > vuex </a>
       </li>
       <li>
         <a
           href="http://vue-loader.vuejs.org/"
           target="_blank"
-        >
-          vue-loader
-        </a>
+        > vue-loader </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/awesome-vue"
           target="_blank"
-        >
-          awesome-vue
-        </a>
+        > awesome-vue </a>
       </li>
     </ul>
   </div>
@@ -85,22 +69,23 @@
 <script>
 export default {
     name: "HelloWorld",
-    data () {
+    data() {
         return {
-            msg: "Welcome to Your Vue.js App 12312"
+            msg: "Welcome to Your Vue.js App 12312",
         };
     },
     methods: {
-        aaa () {
-
+        aaa() {
+            console.log(12312312312);
             console.log(123123);
-        }
-    }
+        },
+    },
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
